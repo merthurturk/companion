ff5dd59b6eb511d26d021e812ca025f2897ef091
Mert Hurturk <mert.hurturk@gmail.com>
2022-12-15 10:20:30 +0300

    Initial Commit

---
+++ companion/companionApp.swift
@@ -0,0 +1,17 @@
+//
+//  companionApp.swift
+//  companion
+//
+//  Created by Mert Hurturk on 15.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct companionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
